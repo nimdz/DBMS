c3e11c0cb829ff578d90280719fc95e99bbdf543
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2022-12-11 16:18:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.4">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styleindex.php"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cinzel|Fauna+One">
+
+
+</head>
+<body>
+    <div class="home">
+        <!-- <h4><a href="http://localhost/System/index.php"> Home </a></h4> -->
+    </div>
+    <div class="content">
+        <h3>Suwa Sahana Hospital (pvt) (Ltd)</h3>
+    </div>
+    <center><div class="wrapper">
+    <div class="levels">
+        <!-- <div class="link1"><a href="Users/Doctor/doctor.php"> Doctor </a></div>
+        <div class="link2"><a href="Users/Nurse/nurse.php"> Nurse </a></div>
+        <div class="link3"><a href="Users/Cleaner/cleaner.php"> Cleaner </a></div>
+        <div class="link4"><a href="Users/Attendent/attendent.php"> Attendent </a></div>
+        <div class="link5"><a href="Users/Patient/patient.php"> Patient </a></div>
+        <div class="link6"><a href="Users/Vendor/vendor.php"> Vendor </a></div>
+        <div class="link6"><a href="Users/Administrator/administrator.php"> Administrator </a></div>
+        <div class="link6"><a href="Users/Registar/registar.php"> Registar </a></div> -->
+
+
+        <div class="link1"><a href="Users/Doctor/doctorlogin.php"> <b>Doctor</b> </a></div>
+        <div class="link2"><a href="Users/Nurse/nurselogin.php"> <b>Nurse</b> </a></div>
+        <div class="link3"><a href="Users/Cleaner/cleanerlogin.php"> <b>Cleaner</b> </a></div>
+        <div class="link4"><a href="Users/Attendent/attendentlogin.php"> <b>Attendent</b> </a></div>
+        <div class="link5"><a href="Users/Patient/patientlogin.php"> <b>Patient</b> </a></div>
+        <div class="link6"><a href="Users/Vendor/vendorlogin.php"> <b>Vendor</b> </a></div>
+        <div class="link6"><a href="Users/Administrator/administratorlogin.php"> <b>Administrator</b> </a></div>
+        <div class="link6"><a href="Users/Registar/registarlogin.php"> <b>Registrar</b> </a></div>
+        
+    </div>
+    </div></center>
+</body>
+</html>
---
+++ styleindex.php
@@ -0,0 +1,86 @@
+<?php
+header('Content-type: text/css;');
+?>
+
+html {
+    height: 100%;
+}
+
+body {
+    margin:0;
+    background-attachment: fixed;
+    /* background-image: linear-gradient(#ffffff, rgb(64, 206, 170)); */
+    background-image: url('images/home.jpg');
+    background-repeat: no-repeat;
+    background-size:cover;
+    background-color:#ffffff71;
+}
+
+.content {
+    
+    text-align: center;
+    font-size: 45px;
+    height: 45%;
+    margin-bottom: 54px;
+}
+
+.levels div {
+    margin: auto;
+    margin-bottom: 10px;
+    text-align: center;
+    background-color: #f1f1f171;
+    opacity: 0.8;
+    width: 40%;
+    border-radius: 10px;
+    
+}
+
+
+
+a:link, a:visited {
+    color: rgb(0, 0, 0);
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+}
+  
+a:hover, a:active {
+    background-color: rgba(5, 102, 5, 0.801);
+    color: white;
+}
+
+a {
+    font-size: 20px;
+    border: 2px solid #73AD21;
+    border-radius: 10px;
+}
+
+h3 {
+    margin: auto;
+    margin-top: 90px;
+    font-family: Cinzel, serif;
+    font-size: 60px;
+}
+
+.home {
+    overflow: hidden;
+    background-color: rgb(63, 30, 30);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    padding:24.5px;
+}
+
+.wrapper{
+    padding: 20px;
+    margin: auto;
+    margin-top: 5cm;
+    background-color: rgba(110, 116, 121, 0.2);
+    width: 50%;
+    align-items: center;
+    height: 100%;
+    border-radius: 15px;
+    
+    margin-top: 0em;
+}
